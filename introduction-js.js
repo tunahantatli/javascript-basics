3d3d049eefbbbcdf97930ae2db7d28e20b1ac480
Tunahan Tatli <tunahantatl@gmail.com>
2022-08-23 13:40:53 +0300

    js-intro

---
+++ introduction-js.js
@@ -0,0 +1,42 @@
+alert("Wellcome to site!")
+//variables
+//Primitive Types
+//a- String
+
+let firstName = "Ricardo";
+console.log(typeof firstName)
+
+//b- number
+let age = 25;
+console.log(typeof age)
+
+//c- boolean
+
+let use_credi= false;
+console.log (typeof use_credi)
+
+//d -undefined
+let phone;
+console.log(typeof phone)
+
+
+//Reference Types : Objects
+
+//a- Array
+let list = ["mick", "sebastian", "max", "lando","carlos"];
+console.log(typeof list)
+
+//b- Object Literals
+let adress = {
+    street: "yapinti",
+    city: "Mut",
+    provience:"Mersin",
+    country:"Turkiye"
+}
+console.log(typeof adress)
+
+//c-function
+var calculate = function(){
+    return 0;
+}
+console.log(typeof calculate)
