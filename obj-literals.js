80a1a955ec304114a7bfc1c81caa903c9b64a94c
Tunahan Tatli <tunahantatl@gmail.com>
2022-08-29 12:18:25 +0300

    final-obj-literals

--- obj-literals.js
+++ obj-literals.js
@@ -1,5 +1,6 @@
 // Objects Literals
 
+let data;
 let user = {
     userName: "tunahantatli-33u",
     firstName : "tunahan",
@@ -7,5 +8,18 @@ let user = {
     age : 25,
     job : "full stack developer",
     school : "clarusway",
-    hobbies : ["coddin","fishing","driving car"]
+    hobbies : ["coddin","fishing","driving car"],
+    address :{
+        city: "Mersin",
+        country: "Turkiye",
+        phone: +905442559987
+    }
 }
+data = user;
+data = user.address;
+data = user.hobbies;
+data = user.hobbies.length;
+data = user.address.city;
+//try all data = 
+console.log(data);
+console.log(typeof user)
