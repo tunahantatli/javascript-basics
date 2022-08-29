aeea8bbb3691322c132e5b7613e0c0a25a6c7eb2
Tunahan Tatli <tunahantatl@gmail.com>
2022-08-29 11:19:40 +0300

    obj-first-commt

---
+++ obj-literals.js
@@ -0,0 +1,11 @@
+// Objects Literals
+
+let user = {
+    userName: "tunahantatli-33u",
+    firstName : "tunahan",
+    lastName : "tatli",
+    age : 25,
+    job : "full stack developer",
+    school : "clarusway",
+    hobbies : ["coddin","fishing","driving car"]
+}
